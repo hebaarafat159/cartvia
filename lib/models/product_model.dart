938b9f2e1215b8547d973fd75b42f18cb5cdf56d
hebaarafat159 <heba.arafat159@gmail.com>
2026-05-12 00:06:32 +0100

    modify product details model

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -2,18 +2,26 @@ class Product {
   final String id;
   final String title;
   final String description;
+  final int quantity;
+  final String measurement;
+  final bool bring;
 
-  Product({
-    required this.id,
-    required this.title,
-    required this.description,
-  });
+  Product(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.quantity,
+      required this.measurement,
+      required this.bring});
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
       id: (json['_id'] ?? json['id'] ?? '').toString(),
       title: (json['title'] ?? 'Untitled course').toString(),
       description: (json['description'] ?? '').toString(),
+      quantity: (json['quantity'] ?? 0),
+      measurement: (json['measurement'] ?? '').toString(),
+      bring: (json['bring'] ?? false),
     );
   }
 }
